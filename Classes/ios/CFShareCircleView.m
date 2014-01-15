ba670f1cd6d07bfac5eeca4c403ce71310535c15
Camden Fullmer <camdenfullmer@gmail.com>
2014-01-14 23:22:56 -0500

    Matched backdrop color to that of iOS UIAlertview.

--- Classes/ios/CFShareCircleView.m
+++ Classes/ios/CFShareCircleView.m
@@ -102,23 +102,9 @@ static const UIWindowLevel UIWindowLevelCFShareCircle = 1999.0;  // Don't overla
         self.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
         self.opaque = NO;
         self.windowLevel = UIWindowLevelCFShareCircle;
+        self.backgroundColor = [UIColor colorWithWhite:0.0 alpha:0.45];
     }
     return self;
-}
-
-- (void)drawRect:(CGRect)rect {
-    CGContextRef context = UIGraphicsGetCurrentContext();
-    size_t locationsCount = 2;
-    CGFloat locations[2] = {0.0f, 1.0f};
-    CGFloat colors[8] = {0.1f, 0.1f, 0.1f, 0.8f, 0.1f, 0.1f, 0.1f, 1.0f};
-    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-    CGGradientRef gradient = CGGradientCreateWithColorComponents(colorSpace, colors, locations, locationsCount);
-    CGColorSpaceRelease(colorSpace);
-    
-    CGPoint center = CGPointMake(self.bounds.size.width / 2, self.bounds.size.height / 2);
-    CGFloat radius = MIN(self.bounds.size.width, self.bounds.size.height) ;
-    CGContextDrawRadialGradient (context, gradient, center, 0, center, radius, kCGGradientDrawsAfterEndLocation);
-    CGGradientRelease(gradient);
 }
 
 @end
